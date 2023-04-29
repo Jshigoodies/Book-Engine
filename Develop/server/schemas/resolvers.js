b82181209d72cff6b14637c550b508f37dd10384
Jason Shi <jshigoodies@gmail.com>
2023-04-28 20:52:21 -0500

    intial commit

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,80 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        getSingleUser: async (_, { user, params }, context) => { //'context' stores the user auth token or status
+            const foundUser = await User.findOne({ //copied from the user-ctonroller.js
+                $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+              });
+
+            if (!foundUser) {
+                throw new Error('Cannot find a user with this id!');
+            }
+    
+            return foundUser;
+        },
+    },
+
+    Mutation: {
+        createUser: async (_, { body }, context) => {
+            const user = await User.create(body);
+
+            if (!user) {
+                throw new Error('Something is wrong!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        login: async (_, { body }, context) => {
+            const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+
+            if (!user) {
+                throw new Error("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(body.password);
+
+            if (!correctPw) {
+                throw new Error('Wrong password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (_, { user, body }, context) => {
+            try {
+                const updatedUser = await User.findOneAndUpdate(
+                  { _id: user._id },
+                  { $addToSet: { savedBooks: body } },
+                  { new: true, runValidators: true }
+                );
+        
+                return updatedUser;
+              } catch (err) {
+                console.log(err);
+                throw new Error(err);
+              }
+        },
+
+        deleteBook: async (_, { user, params }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+            );
+        
+            if (!updatedUser) {
+            throw new Error("Couldn't find user with this id!");
+            }
+        
+            return updatedUser;
+        },
+
+    },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas'); //commented out because i don't have these yet
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+startApolloServer(typeDefs, resolvers);
